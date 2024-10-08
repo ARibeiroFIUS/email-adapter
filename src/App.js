cee70906af0bb486d74c7aec5a6abe19abf3afa7
ARibeiroFIUS <andre.ribeiro@fius.com.br>
2024-10-08 14:31:31 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmailAdapter from './components/EmailAdapter';
+
+function App() {
+  return (
+    <div className="App">
+      <EmailAdapter />
+    </div>
+  );
+}
+
+export default App;
